649552f8cdb2a8839c450bf7c9f3c0049d6823c7
Sofia Martins <work@Sofias-MacBook-Air.local>
2016-12-01 16:45:05 +0000

    Add . Click on button calls ping on subtitles-manager

--- bin/main.js
+++ bin/main.js
@@ -1,11 +1,21 @@
+/**
+ * Module dependencies.
+ */
+
 const config = require('config');
 const electron = require('electron');
 
 const {app} = electron;
 const {BrowserWindow} = electron;
 
+/**
+ * App init.
+ */
+
 app.on('ready', () => {
-    console.log(config.get('assets.bar_icon.dir'));
-    const win = new BrowserWindow({ width: 800, height: 600, icon: config.get('assets.bar_icon.dir') });
+    const win = new BrowserWindow({ width: 800, height: 600 });
+    
     win.loadURL(config.get('templates.main_window.dir'));
+
+    win.webContents.openDevTools();
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 
-const button = document.createElement('button');
-button.textContent = 'Open Window';
+const subtitlesManager = require('../src/open-subtitles/open-subtitles-manager');
+
+
+const titleInput = document.createElement('input');
+titleInput.placeholder = 'File Name';
  
-document.body.appendChild(button);
+document.body.appendChild(titleInput);
+
+
+const searchButton = document.createElement('button');
+searchButton.textContent = 'Search';
+searchButton.addEventListener('click', () => {
+    console.log(titleInput.value);
+    subtitlesManager.ping();
+}, false);
+
+document.body.appendChild(searchButton);
