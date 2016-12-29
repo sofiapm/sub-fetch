b6978346696934bc140f6bffa82d80108cd066f8
Bernardo Simoes <bersimoes@gmail.com>
2016-12-29 15:57:27 +0000

    Enabled copy paste

--- src/menu/menu-template.js
+++ src/menu/menu-template.js
@@ -9,42 +9,44 @@
 
 class MenuTemplate {
 
-    template(){
-        const template = [];
-        template.push(this.editTemplate());
-        template.push(this.helpTemplate());
-        
-        return template;
-    }
+  template(){
+    let template = [];
+    template.push(this.editTemplate());
+    template.push(this.helpTemplate());
+    template.push({ 
+      label: "Quit",
+      accelerator: "Command+Q",
+      click: function() { app.quit(); }
+    });
 
-    editTemplate(){
-        return {
-            label: 'SubFetch',
-            submenu: [
-                {
-                    role: 'about'
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    role: 'quit'
-                }
-            ]
-        };
-    }
+    return template;
+  }
 
-    helpTemplate(){
-        return   {
-            role: 'help',
-            submenu: [
-                {
-                    label: 'Learn More',
-                    click () { require('electron').shell.openExternal('http://electron.atom.io') }
-                }
-            ]
-        };
-    }
+  editTemplate(){
+    return {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]};
+  }
+
+  helpTemplate(){
+    return   {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click () { require('electron').shell.openExternal('http://electron.atom.io') }
+        }
+      ]
+    };
+  }
 }
 
 module.exports = new MenuTemplate();
