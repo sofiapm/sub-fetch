9c306d5f11a9cac46b8056c54b792c56dee7ca06
Bernardo Simoes <bersimoes@gmail.com>
2016-12-29 15:49:43 +0000

    WIP

---
+++ test/open-subtitles/open-subtitles-manager-test.js
@@ -0,0 +1,15 @@
+const subtitlesManager = require('../../src/open-subtitles/open-subtitles-manager.js');
+
+describe('subtitlesManager', function() {
+  const file = {
+
+  };
+
+  describe('#buildHashBestSearchObject', function() {
+    it('parses file info and returns best possible OS hash', function() {
+      subtitlesManager.buildHashBestSearchObject()
+      assert.equal(true, false);
+    });
+  });
+});
+
