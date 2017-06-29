952b282d6d6718ab61df9bfbed6a7dbf1129718d
sofiapm <mps.sofia@gmail.com>
2017-06-29 17:36:37 +0100

    Separate css files. Update `settings.js`: now has multi select with languages from languages_options.json

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,6 +1,19 @@
+const path = require('path')
+const MultiSelect = require(path.resolve('src/html-elements/multi-select'))
+const multiSelect = new MultiSelect()
+const optionsCase = require(path.resolve('db/languages_options.json'))
+
 const { remote } = require('electron')
 const dialog = remote.require('electron').dialog
-// const languages = document.getElementById('languages')
+
+const languagesSelect = document.getElementById('languages-select-label')
+languagesSelect.appendChild(multiSelect.getSelect())
+
+// options from languages_options
+optionsCase.sort(function (a, b) {
+  return a.text.localeCompare(b.text)
+})
+multiSelect.addOptions(optionsCase)
 
 document.getElementById('sub-directory-btn').addEventListener('click', _ => {
   const path = dialog.showOpenDialog({
